776a615b73707db325d15922bfb7554fcf9da0a3
Sofia Tejada <sofianais@icloud.com>
2021-05-05 16:31:59 -0700

    initialize state

--- app.js
+++ app.js
@@ -13,8 +13,20 @@ const wonDisplay = document.querySelector('won-display');
 
 
 // initialize state
+let timesWon = 0;
+let timesLost = 0;
+let timesDraw = 0;
+
+
 
 // set event listeners 
+button.addEventListener('click', () => {
+
+});
+
+
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
