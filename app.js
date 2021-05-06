d8a1773ef11af8fd1b1a214f65cea9eeecda7904
Sofia Tejada <sofianais@icloud.com>
2021-05-05 21:12:02 -0700

    tweak the code a bit

--- app.js
+++ app.js
@@ -43,6 +43,8 @@ button.addEventListener('click', () => {
 
 
     //compare user choice and computer choice by putting it into the function
+
+    //if it's a draw
     if (didUserWin(computerChoice, userChoice) === 'draw') {
 
         //show the draw section
@@ -64,49 +66,57 @@ button.addEventListener('click', () => {
         drawDisplay.textContent = `Wow, the computer chose ${computerChoice}, that's a draw!`;
     } 
     
+    //if the user lost
     else if (didUserWin(computerChoice, userChoice) === 'lose') {
 
         //show the lost section
         drawDisplay.style.display = 'none';
         wonDisplay.style.display = 'none';
         lostDisplay.style.display = 'block';
+
         //update times played
         timesPlayed = timesPlayed + 1;
         timesPlayedSpan.textContent = timesPlayed;
+
         //update times lost
         timesLost = ((timesPlayed) - (timesWon + timesDraw));
         timesLostSpan.textContent = timesLost;
-        console.log(timesLost);
+
         //tell the user what happened
         lostDisplay.textContent = `The computer chose ${computerChoice}. You lose`;
     } 
     
+    //if the user won
     else if (didUserWin(computerChoice, userChoice) === 'win') {
 
         //show the won section
         drawDisplay.style.display = 'none';
         wonDisplay.style.display = 'block';
         lostDisplay.style.display = 'none';
+
         //count the win
         timesWon = timesWon + 1;
+
         //update win count
         timesWonSpan.textContent = timesWon;
+
         //update times played
         timesPlayed = timesPlayed + 1;
         timesPlayedSpan.textContent = timesPlayed;
+
         //tell the user what happened
         wonDisplay.textContent = `The computer chose ${computerChoice}. You win!`;
     }
 });
 
 reset.addEventListener('click', () => {
+
     //reset the displays
     drawDisplay.style.display = 'none';
     wonDisplay.style.display = 'none';
     lostDisplay.style.display = 'none';
 
     //reset the counting spans
-    
     timesPlayedSpan.textContent = '';
     drawsSpan.textContent = '';
     timesLostSpan.textContent = '';
